a21cf73ca48db84ba6091cdc2ea443787473adc2
HoTuan2911 <darkboy2911@gmail.com>
2022-06-17 14:03:08 +0700

    update lan 2

--- LabBigSchool_HoMinhTuan/Controllers/AttendancesController.cs
+++ LabBigSchool_HoMinhTuan/Controllers/AttendancesController.cs
@@ -4,11 +4,35 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using LabBigSchool_HoMinhTuan.Models;
+using Microsoft.AspNet.Identity;
 
 namespace LabBigSchool_HoMinhTuan.Controllers
 {
     public class AttendancesController : ApiController
     {
+        private ApplicationDbContext _dbContext;
 
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int courseId)
+        {
+            var attendance = new Attendance
+            {
+                CourseId = courseId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+
+        
     }
 }
